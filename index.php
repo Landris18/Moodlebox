8593a2a340a6e2b3b07713bdf65704910e538e19
Landry Manankoraisina <47665507+Landris18@users.noreply.github.com>
2022-02-26 23:43:45 +0300

    Sole changes: deletion and change variable

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ if (isset($_POST['passw'])){
 	$mdp = md5($_POST['passw']);
 	$mdp_admin = "24109b73defbf52c9bad6783f37cca52";
 	if ($mdp == $mdp_admin){
-		$diso = "<script>$('#modalConfirmDelete').modal('show');</script>";
+		$message = "<script>$('#modalConfirmDelete').modal('show');</script>";
 	}
 	else{
-		$diso = "Le mot de passe est incorrect";
+		$message = "Le mot de passe est incorrect";
 	}
 }
 
@@ -20,52 +20,34 @@ if (isset($_POST['passw'])){
 	<title>Eteindre</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="images/LOGO-OSP-MENETP.png"/>
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">	
 	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link rel="stylesheet" type="text/css" href="css/modal.css">
-<!--===============================================================================================-->
 
-<!--===============================================================================================-->
-<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+        <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 	<script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
 	<script src="vendor/daterangepicker/moment.min.js"></script>
 	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
 	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 </head>
 <body>
 
 
         <!--Modal: modalConfirmDelete-->
-	<div class="modal fade" id="modalConfirmDelete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal fade" id="modalConfirmDelete" tabindex="-1" role="dialog" aria-labelledby="modalConfirmDelete" aria-hidden="true">
 		<div class="modal-dialog modal-sm modal-notify modal-danger" role="document">
 			<!--Content-->
 			<div class="modal-content text-center">
@@ -93,9 +75,9 @@ if (isset($_POST['passw'])){
 			<!--/.Content-->
 		</div>
 	</div>
-
-
 	<!--Modal: modalConfirmDelete-->
+
+
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
@@ -120,7 +102,7 @@ if (isset($_POST['passw'])){
 
 					<div class="flex-sb-m w-full p-b-30">
 						<p style="color:#ff3547; font-weight:bold; margin-left:18%">
-							<?php echo $diso?>
+							<?php echo $message?>
 						</p>
 					</div>
 
